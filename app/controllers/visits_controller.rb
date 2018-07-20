b5776c4db41e0e4fe7e841736920728def09d73d
github username <github email address>
2018-07-20 14:32:48 +0300

    country add

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -2,8 +2,9 @@
 class VisitsController < ApplicationController
 	
   def index
+  	user = current_user
+  	country = current_user.countries
   	@visit = Visit.find_by(id: params[:id])
-  
   end
 
 end
